8216cf7e40108962c979efbfb1d5d72c8d3f2219
John Kehinde <jkehinde@axxess.com>
2022-03-22 22:14:11 +0100

    Added Context class

---
+++ API Challenge/Data/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using API_Challenge.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Challenge.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        public DbSet<Country> Countries {get; set;}
+        public DbSet<CountryDetail> CountryDetails { get; set; }
+
+    }
+}
--- API Challenge/Data/Models/Country.cs
+++ API Challenge/Data/Models/Country.cs
@@ -6,5 +6,6 @@
         public string CountryCode { get; set; }
         public string Name { get; set; }
         public string CountryIso { get; set; }
+        public CountryDetail CountryDetail { get; set; }
     }
 }
