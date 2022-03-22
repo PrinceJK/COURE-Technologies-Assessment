fbbb393974fb48752187b19a3dde3f9ce07f9a5e
John Kehinde <jkehinde@axxess.com>
2022-03-22 22:31:25 +0100

    added in memore db

---
+++ API Challenge/Data/Seeder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace API_Challenge.Data
+{
+    public class Seeder
+    {
+        public static Task SeedData()
+        {
+            var options = new DbContextOptions<ApiContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+        }
+    }
+}
