7e93b131b07461f568ce682fb0161638e5b8319d
John Kehinde <jkehinde@axxess.com>
2022-03-22 21:37:05 +0100

    Add project files.

---
+++ Algothm Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algothm_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int SortArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    
+                }
+            }
+            return 0;
+        }
+    }
+}
