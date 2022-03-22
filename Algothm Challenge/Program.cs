45b1e963333a37ef17888e06f5066575c3695446
John Kehinde <jkehinde@axxess.com>
2022-03-22 21:54:31 +0100

    Solved Algotithm challenge

--- Algothm Challenge/Program.cs
+++ Algothm Challenge/Program.cs
@@ -6,19 +6,37 @@ namespace Algothm_Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var intArray = new int[5]{ 1, 2, 3, 4, 5 };
+            var soln = SortArray(intArray);
+            Console.WriteLine(soln);
         }
 
         static int SortArray(int[] arr)
         {
-            for (int i = 0; i < arr.Length; i++)
+            if (arr.Length == 0)
             {
-                if (arr[i] % 2 == 0)
-                {
-                    
-                }
+                return 0;
             }
-            return 0;
+
+            var result = 0;
+            foreach (var number in arr)
+            {
+                if (number == 8)
+                {
+                    result += 5;
+                    continue;
+                }
+                else if (number % 2 == 0)
+                {
+                    result += 1;
+                }
+                else if (number % 2 == 1)
+                {
+                    result += 3;
+                }
+                continue;
+            }
+            return result;
         }
     }
 }
